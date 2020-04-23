9465112a779fb99b3c88e518a188f69572db36d0
Maksym Tkachenko <mytkachenko@yandex.ua>
2020-04-23 21:41:59 +0300

    add popup markup

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// // Let's find the form in the DOM
+// // Let's find the popup in the DOM
 // let formElement = // Use the querySelector() method
 //
-// // Next is the form submit handler, though
+// // Next is the popup submit handler, though
 // // it won't submit anywhere just yet
 //     function formSubmitHandler (evt) {
-//         evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-//         // Having done so, we can define our own way of submitting the form.
+//         evt.preventDefault(); // This line stops the browser from submitting the popup in the default way.
+//         // Having done so, we can define our own way of submitting the popup.
 //         // We'll explain it in more detail later.
 //
-//         // Let's find the form fields in the DOM
+//         // Let's find the popup fields in the DOM
 //         let nameInput = // Use querySelector()
 //             let jobInput = // Use querySelector()
 //
@@ -20,6 +20,6 @@
 //         // Insert new values using the textContent property of the querySelector() method
 //     }
 //
-// // Connect the handler to the form:
+// // Connect the handler to the popup:
 // // it will watch the submit event
 // formElement.addEventListener('submit', formSubmitHandler);

da2bae839a3f00211953e58a33af654ddf47398f
Maksym Tkachenko <mytkachenko@yandex.ua>
2020-05-07 13:35:46 +0300

    add text-overflow ellipsis for profile title

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-//Can you give me a bit of advice on what I'm doing wrong?
-// 1. Onclick event is working, but not removing class with popup_opened
-// when I'm pressing the close button.
-// 2. Save is working but not replacing text in profile section
+const formElement = document.querySelector('.popup');
+const formEdit = document.querySelector('.popup__container');
 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let profileEdit = document.querySelector('.profile__edit-button');
-let popupClose = document.querySelector('.popup__close-icon');
-let formElement = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__profile-title');
-let jobInput = document.querySelector('.popup__profile-subtitle');
+const profileEdit = document.querySelector('.profile__edit-button');
+const popupClose = document.querySelector('.popup__close-icon');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+const nameInput = formEdit.querySelector('.popup__profile-title');
+const jobInput = formEdit.querySelector('.popup__profile-subtitle');
 
 
-profileEdit.addEventListener('click', displayPopup);
-popupClose.addEventListener('click', displayPopup);
+    function displayPopup(evt) {
+        evt.stopPropagation();
 
-    function displayPopup() {
+        if (formElement.classList.contains('popup_closed')) {
+            nameInput.value = profileTitle.textContent;
+            jobInput.value = profileSubtitle.textContent;
+        }
+
         formElement.classList.toggle('popup_opened')
     }
+
     function formSubmitHandler (evt) {
         evt.preventDefault();
 
-        let tmpName = nameInput.value;
-        let tmpJob = jobInput.value;
-
-        profileTitle = tmpName.textContent;
-        profileSubtitle = tmpJob.textContent;
+        profileTitle.textContent = nameInput.value;
+        profileSubtitle.textContent = jobInput.value;
 
         displayPopup()
     }
 
 formElement.addEventListener('submit', formSubmitHandler);
+profileEdit.addEventListener('click', displayPopup);
+popupClose.addEventListener('click', displayPopup);

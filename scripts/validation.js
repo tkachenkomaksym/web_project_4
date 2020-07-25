e7025c067a641c8e743d3fae93804b6b88dc85c0
Maksym Tkachenko <mytkachenko@yandex.ua>
2020-07-26 00:55:19 +0300

    WIP formValidation

---
+++ scripts/validation.js
@@ -0,0 +1,59 @@
+const showErrorMessage = (form, input, rest) => {
+    const error = form.querySelector(`#${input.id}-error`);
+
+    error.textContent = input.validationMessage;
+    error.classList.add('errorClass');
+}
+
+const hideErrorMessage = (form, input, rest) => {
+    const error = form.querySelector(`#${input.id}-error`);
+
+    error.classList.remove('errorClass');
+    error.textContent = '';
+}
+const checkInputValidation = (form, input, rest) => {
+    if (input.validity.valid) {
+        hideErrorMessage(form, input, rest)
+    } else {
+        showErrorMessage(form, input, rest);
+    }
+}
+
+const toggleButtonState = (inputs, submitButton, {inactiveButtonClass}) => {
+    const isValid = inputs.every((input) => input.validity.valid);
+
+    if(isValid){
+        submitButton.classList.remove(inactiveButtonClass)
+    } else {
+        submitButton.classList.add(inactiveButtonClass)
+    }
+}
+
+const enableValidation = ({formSelector, inputSelector, submitButtonSelector, ...rest}) => {
+    const forms = Array.from(document.querySelectorAll(formSelector));
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+        });
+
+        const inputs = Array.from(document.querySelectorAll(inputSelector));
+        const submitButton = document.querySelectorAll(submitButtonSelector);
+
+        inputs.forEach((input) => {
+            input.addEventListener('input', () => {
+                checkInputValidation(form, input, rest);
+                toggleButtonState(inputs, submitButton, rest)
+            })
+        })
+    })
+}
+
+enableValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".popup__submit",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible"
+});

0d8e162a7c663395085c18216ae76f1abbeca3db
Maksym Tkachenko <mytkachenko@yandex.ua>
2020-08-27 15:52:27 +0300

    fix: add type module to script

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import FormValidator from "./FormValidator";
-import Card from "./Card";
+import FormValidator from "./FormValidator.js";
+import Card from "./Card.js";
 
 const defaultConfig = {
     formSelector: ".popup__form",
